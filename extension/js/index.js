e95822190e0f364f765a98b94c35a0421f37e20b
anvisha <anvisha@gmail.com>
2013-09-22 22:07:32 -0400

    tabs

--- extension/js/index.js
+++ extension/js/index.js
@@ -1,9 +1,14 @@
 console.log("hi!");
 chrome.history.onVisited.addListener(processHistory);
+chrome.tabs.onCreated.addListener(processTabs);
 
 function processHistory(result) {
 	console.log(result);
     }
+
+function processTabs(result) {
+	console.log(result);
+}
 
 
 
